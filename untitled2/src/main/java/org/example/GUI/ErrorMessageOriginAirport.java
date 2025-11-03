493643c1713f9f48458452b6a2c346097768ced7
andreasmartenss <amarte24@student.aau.dk>
2025-11-03 15:12:32 +0100

    Jeg er i zonen. Ny errorpage tilfølget

--- untitled2/src/main/java/org/example/GUI/ErrorMessageOriginAirport.java
+++ untitled2/src/main/java/org/example/GUI/ErrorMessageOriginAirport.java
@@ -4,23 +4,17 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
-import org.bytedeco.javacv.FrameGrabber;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
 
-import java.awt.*;
 import java.net.URL;
 
-public class ErrorMessagePage extends Application {
+public class ErrorMessageOriginAirport extends Application {
 
     public static void main(String[] args) {
         launch(args);
