bf1971a125132a9b1779f39ac77ec02590d3f7f6
UngTobias <113026451+UngTobias@users.noreply.github.com>
2025-11-03 12:26:04 +0100

    Return and retrieve UI
    :)

---
+++ untitled2/src/main/java/org/example/GUI/DeliverHP.java
@@ -0,0 +1,76 @@
+package org.example.GUI;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class DeliverHP extends Application {
+
+    private static final String CHECKMARK_ICON_PATH = "images/accept.png";
+    private static final String ARROW_ICON_PATH = "images/next.png";
+
+    @Override
+    public void start(Stage stage) {
+        BorderPane root = new BorderPane();
+        root.setPadding(new Insets(40));
+        root.setStyle("-fx-background-color: white; -fx-border-color: black; -fx-border-width: 5; -fx-border-radius: 15;");
+
+        VBox centerBox = new VBox(20);
+        centerBox.setAlignment(Pos.CENTER);
+
+        ImageView checkmark = loadImageView(CHECKMARK_ICON_PATH, 80);
+        StackPane checkmarkPane = new StackPane();
+        checkmarkPane.getChildren().addAll(checkmark);
+
+        Label thankYouLabel = new Label("Thank you!");
+        thankYouLabel.setFont(Font.font("Arial", 60));
+        thankYouLabel.setTextFill(Color.BLACK);
+
+        Label instructionLabel = new Label("Please drop your\nheadphones to the right");
+        instructionLabel.setFont(Font.font("Arial", 38));
+        instructionLabel.setTextFill(Color.GRAY);
+        instructionLabel.setAlignment(Pos.CENTER);
+        instructionLabel.setWrapText(true);
+
+        centerBox.getChildren().addAll(checkmarkPane, thankYouLabel, instructionLabel);
+
+        root.setCenter(centerBox);
+
+        ImageView arrow = loadImageView(ARROW_ICON_PATH, 80);
+        VBox arrowBox = new VBox(arrow);
+        arrowBox.setAlignment(Pos.CENTER_RIGHT);
+        arrowBox.setPadding(new Insets(0, 40, 0, 0));
+        root.setRight(arrowBox);
+
+        Scene scene = new Scene(root, 1920, 1080);
+        stage.setTitle("Retrieve Headphones");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    private ImageView loadImageView(String path, double size) {
+        Image image;
+        try {
+            image = new Image(getClass().getResource("/" + path).toExternalForm());
+        } catch (Exception e) {
+            image = new Image("file:" + path);
+        }
+        ImageView iv = new ImageView(image);
+        iv.setFitWidth(size);
+        iv.setPreserveRatio(true);
+        return iv;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
