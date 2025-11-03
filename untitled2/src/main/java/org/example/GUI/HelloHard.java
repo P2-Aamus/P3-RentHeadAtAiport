dce7f5afd4396abb80e77b2cb6718bdc509d2b80
miceraalessandro <micera.alex@gmail.com>
2025-11-03 15:46:49 +0100

    Hello hard implemented

--- untitled2/src/main/java/org/example/GUI/HelloHard.java
+++ untitled2/src/main/java/org/example/GUI/HelloHard.java
@@ -11,12 +11,22 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.example.BoardingPass;
+import org.example.Database;
+import org.example.Kiosk;
 
 import java.net.URL;
+import java.sql.SQLException;
 
 public class HelloHard {
 
+    static BoardingPass BP = UIManager.boardingPass;
+    private static Text useCaseText = new Text();
+
     public static Scene createScene(){
+
+        setInstructionMode(Kiosk.useCase);
+
         BorderPane border = new BorderPane();
 
         CircelCheckmarkIcon check = new CircelCheckmarkIcon(500, 500, 500);
@@ -26,7 +36,7 @@ public class HelloHard {
         Text helloText = new Text("Hello!");
         helloText.setFont(Font.font(75));
 
-        Text nameText = new Text("Name Lastname");
+        Text nameText = new Text(BP.getPsgName());
         nameText.setFont(Font.font(50));
 
         // SAS-logo and flight number
@@ -43,20 +53,20 @@ public class HelloHard {
             airlineHBox.getChildren().add(sasView);
         }
 
-        Text flightNumber = new Text("SK2864");
+        Text flightNumber = new Text(BP.getfltNr());
         flightNumber.setFont(Font.font(40));
         airlineHBox.getChildren().add(flightNumber);
 
-        Text originCode = new Text("CPH/EKCH");
+        Text originCode = new Text(BP.getOriginAirport());
         originCode.setFont(Font.font(40));
 
-        Text originFull = new Text("Copenhagen Kastrup");
+        Text originFull = new Text(Database.getNameFromICAO(BP.getOriginAirport()));
         originFull.setFont(Font.font(25));
 
-        Text destCode = new Text("BGO/ENBR");
+        Text destCode = new Text(BP.getDestinationAirport());
         destCode.setFont(Font.font(40));
 
-        Text destFull = new Text("Bergen Flesland");
+        Text destFull = new Text(Database.getNameFromICAO(BP.getDestinationAirport()));
         destFull.setFont(Font.font(25));
 
         VBox originBox = new VBox(originCode, originFull);
@@ -76,13 +86,27 @@ public class HelloHard {
 
         // Use UIButton for custom blue buttons with text layered using StackPane
         UIButton notYourFlightBtn = new UIButton(0, 0);
+        notYourFlightBtn.setOnMouseClicked(event -> {
+            try {
+                Database.deleteLastBP(BP);
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+            UIManager.changeScene(Scanner::createScene);
+        });
+
+
         UIButton continueBtn = new UIButton(0, 0);
+        continueBtn.setOnMouseClicked(e ->{
+            useCaseBtn(Kiosk.useCase);});
+
+
 
         Text notYourFlightText = new Text("Not your flight?");
         notYourFlightText.setFont(Font.font(24));
         notYourFlightText.setFill(Color.WHITE);
 
-        Text continueText = new Text("Continue to payment");
+        Text continueText = useCaseText;
         continueText.setFont(Font.font(24));
         continueText.setFill(Color.WHITE);
 
@@ -101,6 +125,30 @@ public class HelloHard {
         centerContent.setPadding(new Insets(72, 50, 110, 50));
         border.setCenter(centerContent);
 
+
+
         return new Scene(border, 1920, 1080);
+    }
+
+    public static void setInstructionMode(Kiosk.InstructionMode mode) {
+        switch (mode) {
+            case DROP_OFF:
+                useCaseText.setText("Continue to drop-off");
+                break;
+            case PICK_UP:
+                useCaseText.setText("Continue to payment");
+                break;
+        }
+    }
+
+    public static void useCaseBtn(Kiosk.InstructionMode mode) {
+        switch (mode) {
+            case DROP_OFF:
+                UIManager.changeScene(BadScan::createScene);
+                break;
+            case PICK_UP:
+                UIManager.changeScene(Payment::createScene);
+                break;
+        }
     }
 }
