164987124426bcb64919f850b31c2f0a3433d26f
smedanton <smedanton@gmail.com>
2025-11-04 10:30:41 +0100

    lorte fly

---
+++ untitled2/src/main/java/org/example/GUI/hpDroppedOff.java
@@ -0,0 +1,67 @@
+package org.example.GUI;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+
+public class hpDroppedOff extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Scene scene = createScene();
+        primaryStage.setTitle("Headphones dropped off");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static Scene createScene() {
+        BorderPane root = new BorderPane();
+
+
+        Text hpDrop = new Text("Headphones dropped off");
+        hpDrop.setFont(Font.font(75));
+
+
+        Text seeU = new Text("We hope to see you again soon");
+        seeU.setFont(Font.font(50));
+
+        ImageView planeView = null;
+        URL imgUrl = hpDroppedOff.class.getResource("/images/airplane-aircraft-icon-a5-landing-clip-art-landing-2d0cf51950a7e2fd5bd7b9f93a890e1a.png");
+        if (imgUrl != null) {
+            planeView = new ImageView(new Image(imgUrl.toExternalForm()));
+            planeView.setFitHeight(300);
+            planeView.setPreserveRatio(true);
+        }
+
+       VBox vbox2 = new VBox(hpDrop);
+        vbox2.setAlignment(Pos.CENTER);
+        vbox2.setPadding(new Insets(0,0,60,0));
+
+
+
+        VBox vbox = new VBox(50);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setPadding(new Insets(20,0,30,0));
+        vbox.getChildren().addAll(vbox2, seeU);
+        if (planeView != null) {
+            vbox.getChildren().add(planeView);
+        }
+
+        root.setCenter(vbox);
+        return new Scene(root, 1920, 1080);
+    }
+}
---
+++ untitled2/src/main/java/org/example/GUI/pleasantFlight.java
@@ -0,0 +1,67 @@
+package org.example.GUI;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+
+public class pleasantFlight extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Scene scene = createScene();
+        primaryStage.setTitle("Pleasant flight");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static Scene createScene() {
+        BorderPane root = new BorderPane();
+
+
+        Text pleasent = new Text("Have a pleasant flight!");
+        pleasent.setFont(Font.font(75));
+
+
+        Text remember = new Text("Remember to drop your headphones off in Bergen");
+        remember.setFont(Font.font(50));
+
+        ImageView planeUp = null;
+        URL imgUrl = hpDroppedOff.class.getResource("/images/airplane-aircraft-takeoff-take-off-clip-art-plane-b59ead33bdd3e093bae1972ae8d89e05.png");
+        if (imgUrl != null) {
+            planeUp = new ImageView(new Image(imgUrl.toExternalForm()));
+            planeUp.setFitHeight(300);
+            planeUp.setPreserveRatio(true);
+        }
+
+        VBox vbox2 = new VBox(pleasent);
+        vbox2.setAlignment(Pos.CENTER);
+        vbox2.setPadding(new Insets(0,0,60,0));
+
+
+
+        VBox vbox = new VBox(50);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setPadding(new Insets(20,0,30,0));
+        vbox.getChildren().addAll(vbox2, remember);
+        if (planeUp != null) {
+            vbox.getChildren().add(planeUp);
+        }
+
+        root.setCenter(vbox);
+        return new Scene(root, 1920, 1080);
+    }
+}
