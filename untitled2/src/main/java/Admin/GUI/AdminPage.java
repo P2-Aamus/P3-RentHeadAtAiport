ec3ab48ef95a48f6b2b3f5195949fecc8d73d057
andreasmartenss <amarte24@student.aau.dk>
2025-11-05 13:54:05 +0100

    Update AdminPage.java

--- untitled2/src/main/java/Admin/GUI/AdminPage.java
+++ untitled2/src/main/java/Admin/GUI/AdminPage.java
@@ -2,9 +2,17 @@ package Admin.GUI;
 
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import java.util.*;
+import org.example.GUI.UIButton;
 
 
 
@@ -17,7 +25,7 @@ public class AdminPage extends Application {
 
     public void start(Stage primaryStage) {
         Scene scene = createScene();
-        primaryStage.setTitle("Headphones dropped off");
+        primaryStage.setTitle("Admin");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -25,8 +33,34 @@ public class AdminPage extends Application {
     public static Scene createScene() {
         BorderPane root = new BorderPane();
 
+        Text overview = new Text("Overview");
+        overview.setFont(new Font(50));
+
+        Text Graph = new Text("Graph");
+        Graph.setFont(new Font(50));
+
+        UIButton downloadButton = new UIButton(100, 100);
+         downloadButton.setScaleX(0.5);
+         downloadButton.setScaleY(0.5);
 
 
+        HBox buttonBox = new HBox(downloadButton);
+        buttonBox.setPadding(new Insets(0,0,0,1000));
+        root.setTop(buttonBox);
+
+
+        HBox graphBox = new HBox(Graph);
+        graphBox.setPadding(new Insets(70, 500, 40, 100));
+
+
+        VBox overviewBox = new VBox(overview);
+        overviewBox.setPadding(new Insets(70,40,40,20));
+
+
+        HBox centerBox = new HBox();
+        root.setCenter(centerBox);
+
+        centerBox.getChildren().addAll(graphBox, overviewBox);
 
 
 
